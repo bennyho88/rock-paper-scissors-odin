dc368b46824d685db13e0fa56cf1620d3d492784
Benny Ho <benny.ho2530@gmail.com>
2020-04-08 12:31:38 +0200

    added game function

--- app.js
+++ app.js
@@ -1,53 +1,83 @@
 
+let playerScore = 0;
+let computerScore = 0;
 
+// gets random option for computer selection 
 function computerPlay() {
 
     // returns randomly rock, paper or scissors
     const option = ['rock', 'paper', 'scissors'];
 
     let random = Math.floor(Math.random() * 3);
-    
+
     return option[random];
 
 }
 
-computerPlay();
+// computerPlay();
 
 const computerSelection = computerPlay();
 
+// input option for player
 const option = "RoCk"
 const playerSelection = option.toLowerCase();
 
-function playGame(playerSelection, computerSelection) {
 
-    console.log('Player plays: ' + playerSelection);
-    console.log('Computer plays: ' +computerSelection);
+// play 1 round
+function playRound(playerSelection, computerSelection) {
 
-    if(playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('Player wins with rock');
-    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('Computer wins with paper');
+     console.log('Player plays: ' + playerSelection);
+     console.log('Computer plays: ' + computerSelection);
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
+        return 'Player wins with rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
+        return 'Computer wins with paper';
     } else if (playerSelection === 'rock' && computerSelection === "rock") {
-        console.log('equal game');
+        return 'equal game';
     }
 
-    if(playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('Player wins with paper');
-    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('Computer wins with scissors');
+    if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
+       return 'Player wins with paper';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
+        return 'Computer wins with scissors';
     } else if (playerSelection === 'paper' && computerSelection === "paper") {
-        console.log('equal game');
+        return 'equal game';
     }
 
-    if(playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log('Player wins with scissors');
-    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('Computer wins with rock');
+    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
+        return 'Player wins with scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
+        return 'Computer wins with rock';
     } else if (playerSelection === 'scissors' && computerSelection === "scissors") {
-        console.log('equal game');
+        return 'equal game';
     }
-  
 
 }
 
-playGame(playerSelection, computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+
+// function for play game 5 times 
+
+function game() {
+
+    let playerTurn = prompt('Please enter your option!');
+
+    playRound(playerTurn, computerPlay());
+    playRound(playerTurn, computerPlay());
+    playRound(playerTurn, computerPlay());
+    playRound(playerTurn, computerPlay());
+    playRound(playerTurn, computerPlay());
+
+}
+
+game()
+
+
